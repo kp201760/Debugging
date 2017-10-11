8e5d281bb1444caf42deedb9f528139a8bd8db12
kp201760 <30428224+kp201760@users.noreply.github.com>
2017-10-11 14:44:47 +1000

    Code Implementation
    # Throw exception if age is less than 18
    # Throw exception if balance is less than 0 and greater than 200

--- CrownAndAnchorGame/src/src/Player.java
+++ CrownAndAnchorGame/src/src/Player.java
@@ -2,13 +2,16 @@
 public class Player {
 	private String name;
 	private int balance;
+	int age;
 	private int limit;
 	
-	public Player(String name, int balance) {
+	public Player(String name, int balance, int age) {
 		if (name == null || name .isEmpty()) throw new IllegalArgumentException("Name cannot be null or empty");
-		if (balance < 0) throw new IllegalArgumentException("Balance cannot be negative");
+		if (balance < 0 && balance > 200) throw new IllegalArgumentException("Balance cannot be negative and more than 200");
+		if (age < 18) throw new IllegalArgumentException("Age cannot be less than 18");
 		this.name = name;
 		this.balance = balance;
+		this.age=age;
 		this.limit = 0;
 	}
 		
@@ -31,7 +34,7 @@ public class Player {
 	}
 	
 	public void takeBet(int bet) {
-		if (bet < 0) throw new IllegalArgumentException("Bet cannot be negative.");
+		if (bet < 0 && bet > 100 ) throw new IllegalArgumentException("Bet cannot be negative.");
 		if (!balanceExceedsLimitBy(bet)) throw new IllegalArgumentException("Placing bet would go below limit.");
 		balance = balance - bet;
 	}
