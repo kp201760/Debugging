ac881e000a942d716617ed6429be8a392cee7fa7
kp201760 <30428224+kp201760@users.noreply.github.com>
2017-10-13 21:01:55 +1000

    Comments implementation

--- CrownAndAnchorGame/src/src/Game.java
+++ CrownAndAnchorGame/src/src/Game.java
@@ -27,7 +27,7 @@ public class Game {
 	public int playRound(Player player, DiceValue pick, int bet ) {		
 		if (player == null) throw new IllegalArgumentException("Player cannot be null.");
 		if (pick == null) throw new IllegalArgumentException("Pick cannot be negative.");
-		if (bet < 0) throw new IllegalArgumentException("Bet cannot be negative.");
+		if (bet < 0) throw new IllegalArgumentException("Bet cannot be negative.");//Set limit to greater than zero
 		
 		player.takeBet(bet);
 		    
